38d2cd2b71840648a0d23629bad9514a918b3136
larsencs <quietus77@gmail.com>
2022-03-13 18:25:46 -0500

    initial commit

---
+++ src/main/java/README.md
@@ -0,0 +1,5 @@
+## About The Project
+This was an assignment given during undergrad.
+
+### Purpose
+The purpose of this assignment was to practice switch statements in Java. This app takes input from a user, a number from 1 to 10, and then returns that number in roman numeral format in command line.
---
+++ src/main/java/romanNumerals.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class romanNumerals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number in the range of 1 - 10: ");
+        int num = scan.nextInt();
+        
+        switch(num){
+            case 1:
+                System.out.println("I");
+                break;
+            case 2:
+                System.out.println("II");
+                break;
+            case 3:
+                System.out.println("III");
+                break;
+            case 4:
+                System.out.println("IV");
+                break;
+            case 5:
+                System.out.println("V");
+                break;
+            case 6:
+                System.out.println("VI");
+                break;
+            case 7:
+                System.out.println("VII");
+                break;
+            case 8:
+                System.out.println("VIII");
+                break;
+            case 9:
+                System.out.println("IX");
+                break;
+            case 10:
+                System.out.println("X");
+                break;
+            default:
+                System.out.println("Invalid number.");
+                break;
+        }
+
+        
+    }
+    
+}
